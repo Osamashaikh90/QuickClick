eb377ef7ec8cddddaa2ffc73929502b6f1c4e65a
Osama <shaikhosama499@gmail.com>
2023-07-13 18:12:15 +0530

    Context Page Created and implemented UseReducer hook

---
+++ client/src/context/productContext.jsx
@@ -0,0 +1,57 @@
+//create context api
+//create provider
+//create customer -> useContext hook
+import { useContext, createContext, useEffect, useReducer } from "react";
+import axios from "axios";
+import reducer from "../reducer/productReducer";
+const AppContext = createContext();
+const API = "https://api.pujakaitem.com/api/products";
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  featureProducts: [],
+  isSingleLoading: false,
+  singleProduct: {},
+};
+// eslint-disable-next-line react/prop-types
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  //*For all products
+  const getProducts = async (url) => {
+    dispatch({ type: "SET_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const products = await res.data;
+      dispatch({ type: "SET_API_DATA", payload: products });
+    } catch (error) {
+      dispatch({ type: "API_ERROR" });
+    }
+  };
+
+  //For singleproduct
+  const getSingleProduct = async (url) => {
+    dispatch({ type: "SET_SINGLE_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const singleProduct = await res.data;
+      dispatch({ type: "SET_SINGLE_PRODUCT", payload: singleProduct });
+    } catch (error) {
+      dispatch({ type: "SET_SINGLE_ERROR" });
+    }
+  };
+  useEffect(() => {
+    getProducts(API);
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ ...state, getSingleProduct }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+//Global custom hooks
+const useProductContext = () => {
+  return useContext(AppContext);
+};
+export { AppProvider, AppContext, useProductContext };

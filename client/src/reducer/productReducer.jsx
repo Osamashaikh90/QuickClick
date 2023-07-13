2f6ef0e5ca4ff022e22b7d55f3f944a1dfea2a12
Osama <shaikhosama499@gmail.com>
2023-07-13 18:13:05 +0530

    Reducer for useReducer created

---
+++ client/src/reducer/productReducer.jsx
@@ -0,0 +1,49 @@
+const ProductReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_LOADING":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case "SET_API_DATA":
+      //   eslint-disable-next-line no-case-declarations
+      const featureData = action.payload.filter((curElem) => {
+        return curElem.featured === true;
+      });
+      //   console.log(featureData);
+      return {
+        ...state,
+        isLoading: false,
+        products: action.payload,
+        featureProducts: featureData,
+      };
+
+    case "API_ERROR":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case "SET_SINGLE_LOADING":
+      return {
+        ...state,
+        isSingleLoading: true,
+      };
+    case "SET_SINGLE_PRODUCT":
+      return {
+        ...state,
+        isSingleLoading: false,
+        singleProduct: action.payload,
+      };
+    case "SET_SINGLE_ERROR":
+      return {
+        ...state,
+        isSingleLoading: false,
+        isError: true,
+      };
+
+    default:
+      return state;
+  }
+};
+export default ProductReducer;

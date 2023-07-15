987f3ec9271b708cf4211e0c3c0ff971cc1f4646
Osama <shaikhosama499@gmail.com>
2023-07-15 23:32:29 +0530

    CartAmount Component created and imp to Single product page

---
+++ client/src/Components/CartAmountToggle.jsx
@@ -0,0 +1,48 @@
+// import React from 'react'
+import styled from "styled-components";
+import { FaMinus, FaPlus } from "react-icons/fa";
+const CartAmountToggle = ({ amount, setDecrease, setIncrease }) => {
+  return (
+    <Wrapper className="cart-button">
+      <div className="amount-toggle">
+        <button
+          onClick={() => {
+            setDecrease();
+          }}
+        >
+          <FaMinus />
+        </button>
+        <div className="amount-style">{amount}</div>
+        <button
+          onClick={() => {
+            setIncrease();
+          }}
+        >
+          <FaPlus />
+        </button>
+      </div>
+    </Wrapper>
+  );
+};
+const Wrapper = styled.section`
+.amount-toggle {
+    margin-top: 3rem;
+    margin-bottom: 1rem;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    font-size: 1.4rem;
+
+    button {
+      border: none;
+      background-color: #fff;
+      cursor: pointer;
+    }
+
+    .amount-style {
+      font-size: 2.4rem;
+      color: ${({ theme }) => theme.colors.btn};
+    }
+`;
+
+export default CartAmountToggle;

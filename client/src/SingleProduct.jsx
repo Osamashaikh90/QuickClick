b7a61de348c4a6f04571b97a7d30b8f347785dd4
Osama <shaikhosama499@gmail.com>
2023-07-15 23:32:43 +0530

    SingleProduct Page completed

--- client/src/SingleProduct.jsx
+++ client/src/SingleProduct.jsx
@@ -1,11 +1,15 @@
 // import React from "react";
+import { TbTruckDelivery, TbReplace } from "react-icons/tb";
+import { MdSecurity } from "react-icons/md";
 import styled from "styled-components";
 import { useEffect } from "react";
 import { useProductContext } from "./context/productContext";
 import { useParams } from "react-router-dom";
 import BreadCrumb from "./Components/BreadCrumb";
 import SingleProductImg from "./Components/SingleProductImg";
-import { Container } from "postcss";
+import AddToCart from "./Components/AddToCart";
+import PriceFormator from "./helpers/PriceFormator";
+import Ratings from "./Components/Ratings";
 const API = "https://api.pujakaitem.com/api/products";
 const SingleProduct = () => {
   const { getSingleProduct, isSingleLoading, singleProduct } =
@@ -36,9 +40,67 @@ const SingleProduct = () => {
     <Wrapper>
       <BreadCrumb title={name} />
       <div className="container">
-        <div className="grid grid-two-coumn">
+        <div className="grid grid-two-column">
           <div className="product_images">
-            <SingleProductImg img={image} />
+            <SingleProductImg imgs={image} />
+          </div>
+
+          {/* Product data */}
+          <div className="product-data">
+            <h2>{name}</h2>
+            <Ratings stars={stars} reviews={reviews} />
+
+            <p className="product-data-price">
+              MRP:
+              <del>
+                <PriceFormator price={price + 250000} />
+              </del>
+            </p>
+            <p
+              style={{ marginRight: "5px" }}
+              className="product-data-price product-data-real-price"
+            >
+              Deal of the Day:
+              <PriceFormator price={price} />
+            </p>
+
+            <p>{description}</p>
+            <div className="product-data-warranty">
+              <div className="product-warranty-data">
+                <TbTruckDelivery className="warranty-icon" />
+                <p>Free Delivery</p>
+              </div>
+
+              <div className="product-warranty-data">
+                <TbReplace className="warranty-icon" />
+                <p>15 Days Replacement</p>
+              </div>
+
+              <div className="product-warranty-data">
+                <TbTruckDelivery className="warranty-icon" />
+                <p>Quick Shipment </p>
+              </div>
+
+              <div className="product-warranty-data">
+                <MdSecurity className="warranty-icon" />
+                <p>2 Year Warranty </p>
+              </div>
+            </div>
+
+            <div className="product-data-info">
+              <p>
+                Available:
+                <span> {stock > 0 ? "In Stock" : "Not Available"}</span>
+              </p>
+              <p>
+                ID : <span> {id} </span>
+              </p>
+              <p>
+                Brand :<span> {company} </span>
+              </p>
+            </div>
+            <hr />
+            {stock > 0 && <AddToCart product={singleProduct} />}
           </div>
         </div>
       </div>
@@ -48,15 +110,24 @@ const SingleProduct = () => {
 
 const Wrapper = styled.section`
   .container {
-    padding: 9rem 0;
+    /* padding: 0; */
+    width: 100%;
+    padding: 5rem 0rem;
+  }
+  .product_images {
+    display: flex;
+    align-items: center;
   }
   .product-data {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: center;
-    gap: 2rem;
+    gap: 1rem;
 
+    .grid-two-column {
+      grid-template-columns: repeat(2, 1fr);
+    }
     .product-data-warranty {
       width: 100%;
       display: flex;

519518bc486e9221958939f47ed4da8763f9aac4
Osama <shaikhosama499@gmail.com>
2023-07-17 20:39:24 +0530

    Reducer created for UseReducer for Filter Products

---
+++ client/src/reducer/filterReducer.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+
+const filterReducer = (state, action) => {
+  switch (action.type) {
+    case "LOAD_FILTER_PRODUCTS":
+      return {
+        ...state,
+        filter_products: [...action.payload], //...(desrtucturing coz we dont want to change og data )
+        all_products: [...action.payload],
+      };
+
+    case "SET_GRIDVIEW":
+      return {
+        ...state,
+        grid_view: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default filterReducer;

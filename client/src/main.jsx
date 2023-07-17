f71543d3882e9cadf14835bd7dbc997a53cfb94f
Osama <shaikhosama499@gmail.com>
2023-07-17 20:40:11 +0530

    FilterProducts provider added to main js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AppProvider } from "./context/productContext.jsx";
+import { FilterContextProvider } from "./context/FilterContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <FilterContextProvider>
+        <App />
+      </FilterContextProvider>
     </AppProvider>
   </React.StrictMode>
 );

cf5fef62891249e93967aeb995820aaa1c64e914
Osama <shaikhosama499@gmail.com>
2023-08-12 17:31:23 +0530

    feat(main): cart provider added to pass down data to children

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { AppProvider } from "./context/productContext.jsx";
 import { FilterContextProvider } from "./context/FilterContext.jsx";
+import { CartProvider } from "./context/cartContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProvider>
       <FilterContextProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </FilterContextProvider>
     </AppProvider>
   </React.StrictMode>

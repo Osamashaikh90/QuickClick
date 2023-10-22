6aa31e63cc0b7c0e825e240fcc84db1cc4257388
Osama <shaikhosama499@gmail.com>
2023-10-22 21:08:17 +0530

    test(app): test 6 for production error

--- client/src/reducer/cartReducer.jsx
+++ client/src/reducer/cartReducer.jsx
@@ -98,7 +98,7 @@ const cartReducer = (state, action) => {
     };
   }
 
-  if (action.type === "CART_ITEM_VALUE") {
+  /*if (action.type === "CART_ITEM_VALUE") {
     let updatedItemValue = state.cart.reduce((initialValue, curElem) => {
       let { amount } = curElem;
       initialValue = initialValue + amount;
@@ -120,29 +120,29 @@ const cartReducer = (state, action) => {
       ...state,
       total_price: total_price,
     };
-  }
+  }*/
   //Combining both the anouve reducer in one
-  // if (action.type === "CART_ITEM_PRICE_TOTAL") {
-  //   let { total_item, total_price } = state.cart.reduce(
-  //     (accum, curElem) => {
-  //       let { price, amount } = curElem;
+  if (action.type === "CART_ITEM_PRICE_TOTAL") {
+    let { total_item, total_price } = state.cart.reduce(
+      (accum, curElem) => {
+        let { price, amount } = curElem;
 
-  //       accum.total_item += amount;
-  //       accum.total_price += price * amount;
+        accum.total_item += amount;
+        accum.total_price += price * amount;
 
-  //       return accum;
-  //     },
-  //     {
-  //       total_item: 0,
-  //       total_price: 0,
-  //     }
-  //   );
-  //   return {
-  //     ...state,
-  //     total_item,
-  //     total_price,
-  //   };
-  // }
+        return accum;
+      },
+      {
+        total_item: 0,
+        total_price: 0,
+      }
+    );
+    return {
+      ...state,
+      total_item,
+      total_price,
+    };
+  }
   return state;
 };
 

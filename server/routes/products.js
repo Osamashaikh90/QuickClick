76cf95bc0531e8b43e174501858dcffb311b48bc
Osama <shaikhosama499@gmail.com>
2023-10-30 13:37:26 +0530

    chore(server): routes added for data integration from db

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const upload = require("../middleware/multer");
 
 const {
   getAllProducts,
-  getAllProductsTesting,
+  Create,
+  getSingleProduct,
+  deleteProduct,
+  updateProduct,
+  getQuery,
+  // getAllProductsTesting,
 } = require("../controllers/products");
 //What to show when some one gose to "/" i.e home page
-router.route("/").get(getAllProducts);
-router.route("/testing").get(getAllProductsTesting);
+//*post routes
+// router.route("/create").post(Create, upload.fields([{ name: "image" }]));
+router.post("/create", upload.fields([{ name: "image", maxCount: 1 }]), Create);
+router.route("/get").get(getAllProducts);
+router.route("/").get(getQuery);
+
+//*get routes
+router.route("/get/:id").get(getSingleProduct);
+//*delete routes
+router.route("/delete/:id").delete(deleteProduct);
+//*put routes
+router.route("/update/:id").put(updateProduct);
 
 module.exports = router;

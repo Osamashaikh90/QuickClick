b5b7c582a8b1d772e12dd89f87db7f89012eca03
Osama <shaikhosama499@gmail.com>
2024-03-08 23:53:17 +0530

    chore(app): old footer implemented again

--- client/src/Components/navigation/Footer.jsx
+++ client/src/Components/navigation/Footer.jsx
@@ -1,143 +1,164 @@
-// import React from 'react'
+// // import React from 'react'
 import { styled } from "styled-components";
 import { Button } from "../../styles/Button";
 import { NavLink } from "react-router-dom";
-const Footer = () => {
-  const Wrapper = styled.section`
-    .iSIFGq {
-      margin: 0;
-    }
+import { FaDiscord, FaInstagram, FaYoutube } from "react-icons/fa";
 
+const Footer = () => {
+  return (
+    <>
+      <Wrapper>
+        <section className="contact-short">
+          <div className="grid grid-two-column">
+            <div>
+              <h3>Ready to get started?</h3>
+              <h3>Talk to us today</h3>
+            </div>
+
+            <div>
+              <Button className="btn hireme-btn">
+                <NavLink to="/"> Get Started </NavLink>
+              </Button>
+            </div>
+          </div>
+        </section>
+        {/* footer section */}
+
+        <footer>
+          <div className="container grid grid-four-column">
+            <div className="footer-about">
+              <h3>QuickClick</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+            </div>
+            <div className="footer-subscribe">
+              <h3>Subscribe to get important updates</h3>
+              <form action="#">
+                <input type="email" name="email" placeholder="YOUR E-MAIL" />
+
+                <input type="submit" value="subscribe" />
+              </form>
+            </div>
+            <div className="footer-social">
+              <h3>Follow Us</h3>
+              <div className="footer-social--icons">
+                <div>
+                  <FaDiscord className="icons" />
+                </div>
+                <div>
+                  <FaInstagram className="icons" />
+                </div>
+                <div>
+                  <a
+                    href="https://www.instagram.com/osamashaikh_786/"
+                    >
+                    <FaYoutube className="icons" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="footer-contact">
+              <h3>Call Us</h3>
+              <h3>+91 12345678978</h3>
+            </div>
+          </div>
+
+          <div className="footer-bottom--section">
+            <hr />
+            <div className="container grid grid-two-column ">
+              <p>
+                @{new Date().getFullYear()} QuickCLick. All Rights Reserved
+              </p>
+              <div>
+                <p>PRIVACY POLICY</p>
+                <p>TERMS & CONDITIONS</p>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </Wrapper>
+    </>
+  );
+};
+
+const Wrapper = styled.section`
+  .iSIFGq {
+    margin: 0;
+  }
+
+  .contact-short {
+    max-width: 60vw;
+    margin: auto;
+    padding: 5rem 10rem;
+    background-color: ${({ theme }) => theme.colors.bg};
+    border-radius: 1rem;
+    box-shadow: ${({ theme }) => theme.colors.shadowSupport};
+    transform: translateY(50%);
+
+    .grid div:last-child {
+      justify-self: end;
+      align-self: center;
+    }
+  }
+
+  footer {
+    padding: 14rem 0 9rem 0;
+    background-color: ${({ theme }) => theme.colors.footer_bg};
+    h3 {
+      color: ${({ theme }) => theme.colors.hr};
+      margin-bottom: 2.4rem;
+    }
+    p {
+      color: ${({ theme }) => theme.colors.white};
+    }
+    .footer-social--icons {
+      display: flex;
+      gap: 2rem;
+
+      div {
+        padding: 1rem;
+        border-radius: 50%;
+        border: 2px solid ${({ theme }) => theme.colors.white};
+
+        .icons {
+          color: ${({ theme }) => theme.colors.white};
+          font-size: 2.4rem;
+          position: relative;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+
+  .footer-bottom--section {
+    padding-top: 9rem;
+
+    hr {
+      margin-bottom: 2rem;
+      color: ${({ theme }) => theme.colors.hr};
+      height: 0.1px;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
     .contact-short {
-      max-width: 60vw;
-      margin: auto;
-      padding: 3rem 6rem;
-      background-color: ${({ theme }) => theme.colors.bg};
-      border-radius: 1rem;
-      box-shadow: ${({ theme }) => theme.colors.shadowSupport};
-      transform: translateY(50%);
+      max-width: 80vw;
+      margin: 4.8rem auto;
+      transform: translateY(0%);
+      text-align: center;
 
       .grid div:last-child {
-        justify-self: end;
-        align-self: center;
+        justify-self: center;
       }
     }
 
     footer {
-      padding: 11rem 0 9rem 0;
-      background-color: ${({ theme }) => theme.colors.footer_bg};
-      h3 {
-        color: ${({ theme }) => theme.colors.hr};
-        margin-bottom: 2rem;
-        font-weight: bold;
-        font-size: x-large;
-      }
-      p {
-        color: ${({ theme }) => theme.colors.white};
-      }
-      .footer-social--icons {
-        display: flex;
-        gap: 2rem;
-
-        div {
-          padding: 1rem;
-          border-radius: 50%;
-          border: 2px solid ${({ theme }) => theme.colors.white};
-
-          .icons {
-            color: ${({ theme }) => theme.colors.white};
-            font-size: 2.4rem;
-            position: relative;
-            cursor: pointer;
-          }
-        }
-      }
+      padding: 9rem 0 9rem 0;
     }
 
     .footer-bottom--section {
-      padding-top: 9rem;
-
-      hr {
-        margin-bottom: 1.5rem;
-        color: ${({ theme }) => theme.colors.hr};
-        height: 0.1px;
-      }
+      padding-top: 4.8rem;
     }
-    .grid-four-column {
-      grid-template-columns: repeat(4, 1fr);
-    }
-    @media (max-width: ${({ theme }) => theme.media.mobile}) {
-      .contact-short {
-        max-width: 80vw;
-        margin: 4.8rem auto;
-        transform: translateY(0%);
-        text-align: center;
-
-        .grid div:last-child {
-          justify-self: center;
-        }
-      }
-
-      footer {
-        padding: 8rem 0 8rem 0;
-      }
-
-      .footer-bottom--section {
-        padding-top: 4.8rem;
-      }
-    }
-  `;
-
-  return (
-    <Wrapper>
-      <div className="contact-short">
-        <div className="grid grid-two-column">
-          <div>
-            <h3 style={{ marginBottom: "1rem" }}>Ready to get Started?</h3>
-            <h3>Talk to us today</h3>
-          </div>
-          <div>
-            <Button>
-              <NavLink to="/contact">Get Started</NavLink>
-            </Button>
-          </div>
-        </div>
-      </div>
-      <footer>
-        <div className="container grid grid-four-column">
-          <div className="footer-about">
-            <h3>Osama Shop</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laboriosam provident mollitia.
-            </p>
-          </div>
-          <div className="footer-about">
-            <h3>Categories</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laboriosam provident mollitia.
-            </p>
-          </div>
-          <div className="footer-about">
-            <h3>Informations</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laboriosam provident mollitia.
-            </p>
-          </div>
-          <div className="footer-social">
-            <h3>Connect with Us</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laboriosam provident mollitia.
-            </p>
-          </div>
-        </div>
-        <hr />
-      </footer>
-    </Wrapper>
-  );
-};
+  }
+`;
 
 export default Footer;
